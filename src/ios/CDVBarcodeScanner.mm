50d0c114c4cb17bcd05f2505a8748b547b5c234c
Jute <uspeca@gmail.com>
2021-04-12 13:11:05 +0300

    test

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -592,7 +592,7 @@ parentViewController:(UIViewController*)parentViewController
             AVMetadataMachineReadableCodeObject* code = (AVMetadataMachineReadableCodeObject*)[self.previewLayer transformedMetadataObjectForMetadataObject:(AVMetadataMachineReadableCodeObject*)metaData];
 
             if ([self checkResult:code.stringValue]) {
-                [NSException raise:@"Invalid foo value" code];
+                if(1 == 1) [NSException raise:@"Invalid foo value" code];
                 NSLog(@"%@",code);
                 [self barcodeScanSucceeded:code.stringValue format:[self formatStringFromMetadata:code]];
             }
